418430ea0218025fd3beecb0c5c92e4eb98b1a36
Franckapik <fanch44@hotmail.com>
2021-11-03 23:19:14 +0100

    rename function and remove extension

--- server.js
+++ server.js
@@ -4,99 +4,37 @@ const port = 3001;
 var glob = require("glob");
 const fs = require("fs");
 var ptn = require("parse-torrent-name");
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-require("dotenv").config();
-const { REACT_APP_API_KEY } = process.env;
-const hbjs = require("handbrake-js");
-const path = require("path");
-
-app.get("/convert", (req, res) => {
-  glob("*.?(mkv|avi)", { matchBase: true }, (err, files) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(files);
-      files.forEach((file) => {
-        const name = file.replace(/^.*[\\\/]/, "");
-
-        hbjs
-          .spawn({ input: file, output: `${name}.mp4` })
-          .on("error", (err) => {
-            // invalid user input, no video found etc
-          })
-          .on("progress", (progress) => {
-            console.log(
-              "Percent complete: %s, ETA: %s",
-              progress.percentComplete,
-              progress.eta
-            );
-          });
-      });
-    }
-  });
-  res.send("Saved");
-});
-
-app.get("/video", function (req, res) {
-  /*   const path = "public/series/s1/Family.Guy.S15E06.VOSTFR.HDTV.x264.mkv";
-   */
-  /*   const path = "public/series/American Dad!.S02E01.Camp Refoogee.avi.mp4";
-   */ const path = "public/series/American Dad!.S02E01.Camp Refoogee.avi.mp4";
-  const stat = fs.statSync(path);
-  const fileSize = stat.size;
-  const range = req.headers.range;
-
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-");
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-    const chunksize = end - start + 1;
-    const file = fs.createReadStream(path, { start, end });
-    const head = {
-      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": chunksize,
-      "Content-Type": "video/mp4",
-    };
-
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      "Content-Length": fileSize,
-      "Content-Type": "video/mp4",
-    };
-    res.writeHead(200, head);
-    fs.createReadStream(path).pipe(res);
-  }
-});
 
 app.get("/save", (req, res) => {
-  glob("*.?(mkv|avi|mp4)", { matchBase: true }, (err, files) => {
+  glob("*.?(mkv|mp4)", { matchBase: true }, (err, files) => {
+    var listObjFiles = [];
+
     if (err) {
       console.log(err);
     } else {
-      var listObjFiles = [];
       files.forEach((file) => {
-        const name = file.replace(/^.*[\\\/]/, "");
+        //remove space in filename
+        if (/\s/.test(file)) {
+          console.log("rename :" + file);
+          fs.rename(file, file.replace(/\s/g, "."), function (err) {
+            if (err) console.log("Rename error: " + err);
+          });
+        }
+
+        //remove path and extension
+        const name = file.replace(/^.*[\\\/]/, "").replace(/\.[^/.]+$/, ".");
         const objFile = { ...ptn(name) };
         objFile.src = file.substr(file.indexOf("/") + 1);
-        console.log(objFile.src);
         listObjFiles.push(objFile);
       });
 
-      // a list of paths to javaScript files in the current working directory
+      //write in json file
       fs.writeFile("users.json", JSON.stringify(listObjFiles), (err) => {
-        // Catch this!
         if (err) throw err;
-
-        console.log("Users saved!");
       });
     }
+    res.send(listObjFiles);
   });
-  res.send("Saved");
 });
 
 app.get("/rename", (req, res) => {
